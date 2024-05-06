7675ff400fa2ffa89e82e8f67a59fc7b4c0de005
Azkhal Zavier <152007947+Azkhal20@users.noreply.github.com>
2024-05-06 19:48:07 +0700

    third commit - fix analytics

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// components
+import Banner from './components/Banner';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import About from './components/About';
+import Services from './components/Services';
+import Work from './components/Work';
+import Contact from './components/Contact';
+import { Analytics } from "@vercel/analytics/react"
+
+const App = () => {
+  return (
+    <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+      <Header />
+      <Banner />
+      <Nav />
+      <About />
+      <Services />
+      <Work />
+      <Contact />
+      <Analytics/>
+      {/* <div className='h-[4000px]'></div> */}
+    </div>
+  );
+};
+
+export default App;
